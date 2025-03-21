bbc553cd47ed73ceea9e1206fa37f472c6c68a9a
+ <+>
2025-03-21 12:13:17 +0300

    updated migrations

---
+++ lib/migrations/versions/ee7bfee3b064_autogenerate_freebie_migrations.py
@@ -0,0 +1,24 @@
+"""autogenerate freebie migrations
+
+Revision ID: ee7bfee3b064
+Revises: 5f72c58bf48c
+Create Date: 2025-03-21 12:09:00.961192
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ee7bfee3b064'
+down_revision = '5f72c58bf48c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
